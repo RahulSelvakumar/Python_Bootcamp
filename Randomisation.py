d1f98fee77d16646286ea24c76d5b4281698a740
RahulSelvakumar <rupash8754@gmail.com>
2023-02-04 22:29:06 +0530

    Randomisation of bill payment

--- Randomisation.py
+++ Randomisation.py
@@ -1,5 +1,6 @@
 import random
 names=input('Enter all the names: ').split(',')
 size=len(names)
-n=random.randint(0,size+1)
+print(size)
+n=random.randint(0,size-1)
 print(f"{names[n]} should pay the bill")
