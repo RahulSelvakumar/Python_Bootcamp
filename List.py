866bb7433bed8765cf22332828ddc7cf1c62281f
RahulSelvakumar <rupash8754@gmail.com>
2023-02-09 22:28:01 +0530

    3d List

--- List.py
+++ List.py
@@ -3,3 +3,6 @@ row2=['🀤','🀤','🀤']
 row3=['🀤','🀤','🀤']
 board=[row1,row2,row3]
 print(f"{row1}\n{row2}\n{row3}")
+row,col=input("Enter the row(0-2) and coloum(0-2) value: ").split(" ")
+board[int(row)][int(col)]='x'
+print(f"{row1}\n{row2}\n{row3}")
