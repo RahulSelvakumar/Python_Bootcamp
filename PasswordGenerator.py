68da723a55c378aaddfd35da7f31971328bdeb5b
RahulSelvakumar <rupash8754@gmail.com>
2023-02-13 19:20:03 +0530

    Final Code

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,10 +1,25 @@
-ans=0
-for x in range(1,101):
-    if x%3==0 and x%5==0:
-        print("Fizz Buzz")
-    elif x%3==0:
-        print("Fizz")
-    elif x%5==0:
-        print("Buzz")
-    else:
-        print(x)
+import random
+letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+numbers=['0','1','2','3','4','5','6','7','8','9']
+symbols=['!','@','#','$','%','+','&','*','(',')']
+print("Welcome to password Generator!!")
+ip_letters=int(input("How many letters do you like in your password: \n"))
+ip_symbols=int(input("How many symbols do you like in your password: \n"))
+ip_numbers=int(input("How many numbers do you like in your password: \n"))
+password=[]
+for _ in range(1,ip_letters+1):
+    password.append(random.choice(letters))
+    #print(random_char,end="")
+for _ in range(1,ip_symbols+1):
+    password.append(random.choice(symbols))
+    #print(random_sym,end="")
+for _ in range(1,ip_numbers+1):
+    password.append(random.choice(numbers))
+    #print(random_num,end="")
+
+random.shuffle(password)
+
+newPassword=""
+for i in password:
+    newPassword+=i
+print(f"Your password is {newPassword}")
