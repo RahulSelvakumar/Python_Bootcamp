c1eb9da20843b4420f9d7b44f013ab618566a9f7
RahulSelvakumar <rupash8754@gmail.com>
2023-02-13 18:37:14 +0530

    Fizz Buzz

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,4 +1,10 @@
 ans=0
-for x in range(2,101,2):
-    ans+=x
-print(ans)
+for x in range(1,101):
+    if x%3==0 and x%5==0:
+        print("Fizz Buzz")
+    elif x%3==0:
+        print("Fizz")
+    elif x%5==0:
+        print("Buzz")
+    else:
+        print(x)
