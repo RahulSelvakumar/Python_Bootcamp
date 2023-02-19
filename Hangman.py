0793ea90b5bbbb3ef849093be2fe88baa4cbb977
Rahul <rupash8754@gmail.com>
2023-02-19 23:14:51 +0530

    New

--- Hangman.py
+++ Hangman.py
@@ -2,11 +2,17 @@ import random
 word_List=['apple','orange','bananna']
 word=random.choice(word_List)
 guess=input('Guess a lowercase character: ')
-print(word)
-for letter in word:
-    if letter==guess:
+length=len(word)
+blank="_"*length
+print(blank)
+i=0
+while i<length:
+    if guess in word[i]:
         print("Right Guess")
+        blank[i].replace(blank[i],guess)
     else:
         print("wrong guess")
+    i+=1
+    print(blank)
     
     
